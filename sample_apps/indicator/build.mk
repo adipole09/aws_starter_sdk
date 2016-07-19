e70fb55a459c6b8f855cdfb34f4ed14c3e1bd6a2
adipole09 <adityapole9@gmail.com>
2016-07-19 23:13:00 +0530

    Updated build.mk

--- sample_apps/indicator/build.mk
+++ sample_apps/indicator/build.mk
@@ -1,7 +1,13 @@
-# Copyright (C) 2008-2015, Marvell International Ltd.
+# Copyright (C) 2008-2016 Marvell International Ltd.
 # All Rights Reserved.
+#
 
-exec-y += gpio_demo
-gpio_demo-objs-y :=  src/main.c
+exec-y += indicator
+indicator-objs-y := src/main.c src/wc.c
+indicator-cflags-y := -I$(d)/src -DAPPCONFIG_DEBUG_ENABLE=1
+
+# Applications could also define custom linker files if required using following:
+#indicator-ld-y := /path/to/ldscript
+
 # Applications could also define custom board files if required using following:
-#gpio_demo-board-y := /path/to/boardfile
+#indicator-board-y := /path/to/boardfile
